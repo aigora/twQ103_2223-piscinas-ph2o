5078561098f7b9323a13ee1f4b6267b08ca69abd
alvaroroal <alvaro.roalvarez@alumnos.upm.es>
2023-05-01 13:33:58 +0200

    g

--- PISCINAS pH2O.c
+++ PISCINAS pH2O.c
@@ -3,20 +3,29 @@
 int main(){
 	int conductividad, turbidez, coliformes;
 	float pH;
-	char parametros;
+	char parametros[100],nombreFichero[100];
+	FILE *fentrada, *fsalida;
 	
-	FILE*piscinaspH20;
-	piscinaspH20 = fopen("202201_Lavapies.txt", "r");
+	printf("Introduce el nombre del fichero que contiene los datos\n");
+	scanf("%s",nombreFichero);
 	
-	if (piscinaspH20 == NULL) {
+	fentrada = fopen(nombreFichero,"r"); // leyendo	
+	if (fentrada == NULL) {
 		printf("Error, no puede abrir el fichero.\n");
-		return 0;
-	}
-	/*
-	fscanf(piscinaspH20, "%d", &conductividad);
-	printf("La conductividad es: %d\n", conductividad);
-	*/
+		return 0;}
 	
-	fclose(piscinaspH20);
+	
+	fsalida = fopen("resultados2.txt", "w"); // escibiendo
+		if (fsalida == NULL) {
+		printf("Error en la apertura del fichero de salida\n");
+		return 0;}
+		
+	while(fscanf(fentrada, "%s %f %d %d %d", parametros, &pH,&conductividad, &turbidez,&coliformes)!= EOF) {
+		fprintf(fsalida, "%s\t %.1f\t %d\t %d\t %d\n", parametros, pH, conductividad,turbidez, coliformes);
+		}
+
+	fclose(fentrada);
+	fclose(fsalida);
+
 	return 0;
 }
