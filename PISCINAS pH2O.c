864373d84c9001357454384b0d1574de169c4765
joancrack <juan.mardomingo@alumnos.upm.es>
2023-05-01 12:08:06 +0200

    Update PISCINAS pH2O.c

--- PISCINAS pH2O.c
+++ PISCINAS pH2O.c
@@ -1,9 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(){
+	int conductividad, turbidez, coliformes;
+	float pH;
+	char parametros;
 	
-	printf("Prueba2");
+	FILE*piscinaspH20;
+	piscinaspH20 = fopen("202201_Lavapies.txt", "r");
 	
-	hola
+	if (piscinaspH20 == NULL) {
+		printf("Error, no puede abrir el fichero.\n");
+		return 0;
+	}
+	/*
+	fscanf(piscinaspH20, "%d", &conductividad);
+	printf("La conductividad es: %d\n", conductividad);
+	*/
+	
+	fclose(piscinaspH20);
 	return 0;
 }
